306233d6efbafcf93476d482d843e8316bf5abc0
Scottie Enriquez <scottie@enriquez.xxx>
2021-02-21 13:17:51 -0600

    Adding .NET 5 skeleton

---
+++ SharedResources/src/SharedResources/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SharedResources
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new SharedResourcesStack(app, "SharedResourcesStack");
+            app.Synth();
+        }
+    }
+}
